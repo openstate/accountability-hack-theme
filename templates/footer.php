950c893867b774c0df7adc916f2b443213781a99
Sicco van Sas <sicco@openstate.eu>
2016-06-11 00:13:27 +0200

    Improve logo section

--- templates/footer.php
+++ templates/footer.php
@@ -11,9 +11,9 @@
         </ul>
       </div>
       <div class="pull-right">
-        <a class="right-footer-padding" href="http://openstate.eu/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
         <a class="right-footer-padding" href="http://www.rekenkamer.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/ar_bw.svg'; ?>" alt="Algemene Rekenkamer logo"></a>
         <a class="right-footer-padding" href="https://www.cbs.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/cbs_bw.svg'; ?>" alt="Centraal Bureau voor de Statistiek logo"></a>
+        <a class="right-footer-padding" href="http://openstate.eu/" target="_blank"><img class="right-footer-image-osf" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
         <br>
         <br>
         <a class="right-footer-padding" href="https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/minbzk_bw.svg'; ?>" alt="Ministerie van Binnenlandse Zaken en Koninkrijksrelaties logo"></a>
@@ -33,9 +33,9 @@
         <li><a class="left-footer-padding" href="https://github.com/openstate" target="_blank"><i class="fa fa-github fa-2x"></i></a></li>
         <li><a class="hashtag-font-size" href="https://twitter.com/search?f=tweets&q=#AccHack16" target="_blank">#AccHack16</a></li>
       </ul>
-      <a class="right-footer-padding" href="http://openstate.eu/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
       <a class="right-footer-padding" href="http://www.rekenkamer.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/ar_bw.svg'; ?>" alt="Algemene Rekenkamer logo"></a>
       <a class="right-footer-padding" href="https://www.cbs.nl/" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/cbs_bw.svg'; ?>" alt="Centraal Bureau voor de Statistiek logo"></a>
+      <a class="right-footer-padding" href="http://openstate.eu/" target="_blank"><img class="right-footer-image-osf" src="<?= get_template_directory_uri() . '/dist/images/logo-open-state-black.svg'; ?>" alt="Open State Foundation logo"></a>
       <br>
       <br>
       <a class="right-footer-padding" href="https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties" target="_blank"><img class="right-footer-image" src="<?= get_template_directory_uri() . '/dist/images/minbzk_bw.svg'; ?>" alt="Ministerie van Binnenlandse Zaken en Koninkrijksrelaties logo"></a>
