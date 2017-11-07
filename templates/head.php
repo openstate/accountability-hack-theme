b6b1e97eab4d90ff9d274a552af18344e7832ac9
Sicco van Sas <siccovansas@users.noreply.github.com>
2017-11-07 17:40:16 +0100

    Update Google Analytics trackingcode and add anonymizeIp

--- templates/head.php
+++ templates/head.php
@@ -12,14 +12,13 @@
   <meta property="og:image:height" content="1102" />
   <?php wp_head(); ?>
   <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,300i,400,400i|Roboto:400,400i" rel="stylesheet">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-32274817-9"></script>
   <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-32274817-9', 'auto');
-    ga('send', 'pageview');
-
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    ga('set', 'anonymizeIp', true);
+    gtag('config', 'UA-32274817-9');
   </script>
 </head>
